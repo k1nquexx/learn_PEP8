9b22dc6da68c28eb9080e97cd2c4c6215c9493fc
k1nq <kuzukov.kir@gmail.com>
2026-01-15 11:58:45 +0700

    UPD

---
+++ task_4.py
@@ -0,0 +1,16 @@
+def get_sum():
+    x = 1
+    y = 2
+    return x + y
+
+def max_value(a, b):
+    if a > b:
+        return a
+    else:
+        return b
+
+class Test:
+    def method1(self):
+        pass
+    def method2(self):
+        pass
